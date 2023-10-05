c4e748c6adc7c395bd57746145ffa08b3c246782
wen <xyw0025@gmail.com>
2023-10-04 20:54:02 -0400

    Feat: init framework

---
+++ src/main/java/com/group/cs520/Cs520Application.java
@@ -0,0 +1,13 @@
+package com.group.cs520;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs520Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs520Application.class, args);
+	}
+
+}
