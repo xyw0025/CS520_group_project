21a39e65f6de05413198d60e01d5565dfff66884
Wen Hsin-Yu <xyw0025@gmail.com>
2023-11-22 16:34:34 -0500

    Feat: profile and user relation

--- backend/src/main/java/com/group/cs520/service/UserService.java
+++ backend/src/main/java/com/group/cs520/service/UserService.java
@@ -1,11 +1,13 @@
 package com.group.cs520.service;
 
+import com.group.cs520.model.Profile;
 import com.group.cs520.model.User;
 import com.group.cs520.repository.UserRepository;
 import com.group.cs520.service.JwtUtil;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
@@ -13,6 +15,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.HashMap;
 import java.util.Map;
+import com.group.cs520.service.TypeUtil;
+
 
 @Service
 public class UserService {
@@ -27,6 +31,10 @@ public class UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
 
     public List<User> allUsers() {
         return userRepository.findAll();
@@ -106,5 +114,12 @@ public class UserService {
 
     public Optional<User> singleUserByEmail(String email) {
         return userRepository.findUserByEmail(email);
+    }
+
+    public void setProfile(String user_id, Profile profile) {
+        ObjectId id = TypeUtil.ObjectIdConverter(user_id);
+        User user = this.singleUser(id).orElseThrow(() -> new IllegalStateException("user not found"));
+        user.setProfile(profile);
+        mongoTemplate.save(user);
     }
 }
