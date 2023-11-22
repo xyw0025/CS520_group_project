865ed0e5ab020652711bc9ebada0cfc757ee3445
Wen Hsin-Yu <xyw0025@gmail.com>
2023-11-22 16:33:07 -0500

    Fix: profile add fields

--- backend/src/main/java/com/group/cs520/model/Profile.java
+++ backend/src/main/java/com/group/cs520/model/Profile.java
@@ -12,8 +12,10 @@ import jakarta.validation.constraints.NotBlank;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.HashMap;
+import java.time.LocalDate;
 import java.util.Map;
+
+import com.group.cs520.service.DateUtil;
 
 @Data
 @NoArgsConstructor
@@ -30,7 +32,11 @@ public class Profile {
     private Integer gender;
 
     @NotBlank
+    private LocalDate birthday;
+
+    @NotBlank
     private Integer age;
+
     private String bio;
 
     private Boolean isDeleted;
@@ -41,19 +47,19 @@ public class Profile {
     private List<Preference> preferenceIds;
 
 
-
     public Profile(Map<String, String> profileMap) {
         if (profileMap.get("displayName").isBlank()) {
             throw new IllegalArgumentException("display name cannot be null.");
         }
 
         // should be dryer
-
         this.displayName = profileMap.get("displayName");
         this.bio = profileMap.get("bio");
+        this.birthday = DateUtil.dateFormatter(profileMap.get("birthday"), "yyyy-MM-dd");
         this.gender = Integer.parseInt(profileMap.get("gender"));
         this.age = Integer.parseInt(profileMap.get("age"));
         this.createdTime = Instant.now();
         this.updatedTime = Instant.now();
+        this.isDeleted = false;
     }
 }
