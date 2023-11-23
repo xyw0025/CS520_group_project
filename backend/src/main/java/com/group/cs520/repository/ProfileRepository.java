9f3deb37fcd28895d984276f10b6096b02e58f50
Wen Hsin-Yu <xyw0025@gmail.com>
2023-11-22 23:16:31 -0500

    Feat: preference controller - get all preference

--- backend/src/main/java/com/group/cs520/repository/ProfileRepository.java
+++ backend/src/main/java/com/group/cs520/repository/ProfileRepository.java
@@ -9,11 +9,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProfileRepository extends MongoRepository<Profile, ObjectId> {
-    
 }
