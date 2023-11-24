c5ed79df67aa63ef2ffb73ec11dd546377ab1501
Wen Hsin-Yu <xyw0025@gmail.com>
2023-11-23 23:28:24 -0500

    Fix: change profile apis

--- backend/src/main/java/com/group/cs520/documentation/ProfileApi.java
+++ backend/src/main/java/com/group/cs520/documentation/ProfileApi.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
@@ -24,11 +25,8 @@ public interface ProfileApi {
     @Operation(summary = "Retrieves a single profile by ID")
     ResponseEntity<Profile> getSingleProfile(ObjectId id);
 
-    @Operation(summary = "Updates profile preferences")
-    ResponseEntity<?> updateProfilePreferences(String profileId, Map<String, String> payload);
-
     @Operation(summary = "Updates a profile")
-    ResponseEntity<?> updateProfile(ObjectId id, @RequestBody(description = "Preference payload", required = true, 
+    ResponseEntity<?> updateProfile(@Parameter(description = "profile_id ** or should use user id instead? -> but that'd be not so RESTful.. **") ObjectId id, @RequestBody(description = "Preference payload", required = true, 
             content = @Content(schema = @Schema(implementation = UpdateProfilePayload.class))
         ) Map<String, String> payload);
 
@@ -49,10 +47,10 @@ public interface ProfileApi {
         @Schema(description = "Bio", example = "Loves hiking and outdoor activities.")
         public String bio;
     
-        @Schema(description = "User ID", example = "655d3d90fee7474f900da830")
-        public String userId;
-    
         @Schema(description = "Image URLs", example = "[\"http://example.com/profile1.jpg\", \"http://example.com/profile2.jpg\"]")
         public List<String> imageUrls;    
+
+        @Schema(description = "Preference",  example ="[\"655ec8e65e588b53b18d5885\", \"655ec8dc5e588b53b18d5883\"]")
+        public List<ObjectId> preferences;
     }
 }
