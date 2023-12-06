f62746f798fa9e065947030d6eb98b5eec5650cd
Wen Hsin-Yu <xyw0025@gmail.com>
2023-12-06 18:40:51 -0500

    Chore: change status enum order

--- backend/src/main/java/com/group/cs520/constants/MatchConstants.java
+++ backend/src/main/java/com/group/cs520/constants/MatchConstants.java
@@ -2,7 +2,7 @@
 package com.group.cs520.constants;
 
 public class MatchConstants {
-    public enum STATUS { FAILED, MATCHED, AWAIT }
+    public enum STATUS { AWAIT, MATCHED, FAILED }
     public enum BEHAVIOR { ACCEPT, REJECT }
 }
 
