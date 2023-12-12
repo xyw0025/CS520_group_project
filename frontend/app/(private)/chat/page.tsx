91dd1633f58a14a70764bfccb270a38634bb8aec
Boddy Shen <pohengshen96@gmail.com>
2023-12-11 21:01:25 -0500

    Feat: add ChattingRoomProfile component

--- frontend/app/(private)/chat/page.tsx
+++ frontend/app/(private)/chat/page.tsx
@@ -7,6 +7,7 @@ import { useUserService } from '@/utils';
 import { PaperPlaneIcon } from '@radix-ui/react-icons';
 import ChattingRoomUser from '@/components/ChattingRoomUser';
 import MessageBox from '@/components/MessageBox';
+import ChattingRoomProfile from '@/components/ChattingRoomProfile';
 import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
 
@@ -225,20 +226,8 @@ const Chat = () => {
           )}
         </div>
         {/* end message */}
-        <div className="w-2/5 border-l-2 px-5">
-          <div className="flex flex-col">
-            <div className="font-semibold text-xl py-4">Mern Stack Group</div>
-            <img
-              src="https://source.unsplash.com/L2cxSuKWbpo/600x600"
-              className="object-cover rounded-xl h-64"
-              alt=""
-            />
-            <div className="font-semibold py-4">Created 22 Sep 2021</div>
-            <div className="font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
-              perspiciatis!
-            </div>
-          </div>
+        <div className="w-2/5 border-l-2 px-1">
+          {currentChatUser && <ChattingRoomProfile user={currentChatUser} />}
         </div>
       </div>
     </div>
