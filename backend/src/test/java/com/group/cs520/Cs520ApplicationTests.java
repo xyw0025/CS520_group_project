e5efdfdd748c948d505f0cb170ee66c418e76211
Wen Hsin-Yu <48324248+xyw0025@users.noreply.github.com>
2023-12-13 02:33:54 -0500

    Api test fix - add back application test (#24)
    * Feat: user api test
    
    * Feat: preference, profile and match api test
    
    * Chore: remove redundant file and comment lines
    
    * Fix: add back application test

---
+++ backend/src/test/java/com/group/cs520/Cs520ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.group.cs520;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class Cs520ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
