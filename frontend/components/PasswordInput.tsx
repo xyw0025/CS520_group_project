a795425ccc7400cb442863e77f592a856edfac0b
Wen Hsin-Yu <48324248+xyw0025@users.noreply.github.com>
2023-12-12 23:01:50 -0500

    Feat: reset password (#18)
    * Feat: frontend show/hide password
    
    * Chore: add new line at the end of file
    
    * Chore: placeholder should be optional param
    
    * Feat: reset password

---
+++ frontend/components/PasswordInput.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+
+type PasswordInputProps = {
+    fields: {},
+    placeholder?: string;
+    id?: string;
+};
+
+const PasswordInput = ({
+    fields,
+    placeholder,
+    id
+}: PasswordInputProps) => {
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const { clearErrors } = useForm();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+
+  return (
+    <div className="relative">
+      <Input
+        {...fields}
+        id={id ? id: "password"}
+        type={showPassword ? "text" : "password"}
+        value={password}
+        onChange={(e) => {
+            setPassword(e.target.value);
+            clearErrors();
+        }}
+        placeholder={placeholder}
+      />
+      {password && (
+        <span
+          className="absolute cursor-pointer"
+          onClick={togglePasswordVisibility}
+          style={{ right: '10px', top: '50%', transform: 'translateY(-50%)' }}
+          placeholder={placeholder}
+        >
+          {showPassword ? 'hide' : 'show'}
+        </span>
+      )}
+    </div>
+  );
+}
+
+export { PasswordInput };
